469d3bc8be3be4d863ca295a9d8f6f99db90559b
mostafa <mostafameerzad@gmail.com>
2023-02-16 10:48:19 -0800

    Import API-KEY the right way, get data from the API and Handle the potential errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,61 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import Details from "./components/Details";
 import Search from "./components/Search";
 import Map from "./components/Map";
-import "./styles.css"
-// process.REACT_APP_IPFY_KEY;
-// const API_KEY = process.REACT_APP_IPFY_KEY
 
-function getData(url) {
-  return fetch(url).then(res=>res.json()).then(data=>console.log(data))
-}
-const url = "https://geo.ipify.org/api/v2/country,city,vpn?apiKey="
+import "./styles.css";
+import { ConnectionError } from "./icons";
+
+const apiKey= import.meta.env.VITE_IPFY_KEY
+
 function App() {
-  const position = [ 43.31181, 5.37391];
+  const [position, setPosition] = useState(undefined);
+  const [details, setDetails] = useState([]);
+  const [notConnected, setNotConnected] = useState(false)
 
-  const ipDetails = [
-    { name: "ip address", value: "154.57.201.144"},
-    {
-      name: "location",
-      value:"FR Provence-Alpes-Côte d'Azur"
-    },
-    { name: "timezone", value:"+01:00" },
-    { name: "isp", value: "Trans World Associates (Pvt) Ltd" }
-  ];
-  const logger = (arg) => {
-    console.log(arg);
-  };
+  function getData(searchKey="") {
+    fetch(`https://geo.ipify.org/api/v2/country,city,vpn?apiKey=${apiKey}&ipAddress=`+searchKey).then((res) => res.json()).then(data=>{
+
+      setPosition([`${data.location.lat}`, `${data.location.lng}`]);
+
+      setDetails([
+        { name: "ip address", value:`${data.ip}` },
+        {
+          name: "location",
+          value: `${data.location.country} ${data.location.city}`
+        },
+        { name: "timezone", value: `${data.location.timezone} ` },
+        { name: "isp", value: `${data.isp}` }
+      ]);
+        
+    }).catch(()=>{
+      setDetails([
+        { name: "ip address", value:"" },
+        {name: "location",value: ""},
+        { name: "timezone", value: "" },
+        { name: "isp", value: "" }
+      ])
+      setNotConnected(true)
+    })
+  }
+  useEffect(() => {
+    getData()
+
+  }, []);
+
   return (
     <>
       <header className="header">
         <h1 className="header__title">IP Address Tracker</h1>
-        <Search callback={logger} />
-        <Details info={ipDetails} />
+        <Search callback={getData} />
+        <Details info={details} />
       </header>
-      <main className="main">
-        <Map position={position} />
+      <main className={notConnected ? "main disconnected": "main"}>
+        {notConnected ? <ConnectionError/> : position && <Map position={position}/> }
       </main>
     </>
   );
 }
 
-// function getData (){
-//   const {data} = useQuery(["apiData"], () => {
-//     return fetch("https://geo.ipify.org/api/v2/country,city,vpn?apiKey=")
-//   })
-// }
 export default App;
+
