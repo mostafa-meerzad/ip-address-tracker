b254f870b8e63997537ba8f7729813385b9c6ead
mostafa <mostafameerzad@gmail.com>
2023-02-18 01:49:24 -0800

    Bug fix correct font-weight and correct unique key issue

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 
 function Details({info}) {
 
-  console.log(info);
   return (
     // add a key prop to the details section in order to show new data every time
     // otherwise it breaks
-    <ul className='details' key={info[0].value}>
+    <ul className='details' key={`${info[0]?.value} container`}>
 {
-    info.map(({name, value}) => {
-        return <li className="details__field" key={value}>
+    info.map(({name, value}, index) => {
+        return <li className="details__field" key={value + index}>
             <h2 className="details__title">{name}</h2>
             <p className="details__info">{value}</p> 
         </li>
